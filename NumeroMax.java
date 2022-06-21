f0b1dbb9fd14b4111d3f204b9ae6fcb3dbc1d102
Palen1993 <jorgepalen93@gmail.com>
2022-06-21 10:09:13 +0200

    conflicto

---
+++ NumeroMax.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numeromax;
+
+/**
+ *
+ * @author JorgePalenciaGarcía
+ */
+public class NumeroMax {
+
+       public static void multiplos(int numeroMax){
+        
+        for (int i=0;i<=numeroMax;i++){
+            if(i%7==0){
+            System.out.println("Múltiplo de 7"+i);            
+            }            
+            if(i%49==0){
+            System.out.println("Múltiplo de 49"+i);
+        
+        }
+        
+        else if(i%100==0){
+        
+        System.out.println("Múltiplo de 100"+i);
+        
+      }
+    }
+  }
+}
+        
+        
+       
+    
+    
